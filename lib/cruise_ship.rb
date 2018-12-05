aa92bf9347556c4cea594065e043b31453919b80
Cristian Leiva <cleiva94.cl@gmail.com>
2018-12-05 10:02:34 -0500

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,20 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
+  passengers.each do |key, value|
+    if key == :suite_a
+      return value
+    elsif value.include? == "a"
+      return value
+    end
+
+
+  end
 end
